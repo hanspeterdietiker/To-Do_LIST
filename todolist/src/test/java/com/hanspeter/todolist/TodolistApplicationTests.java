7ca4dd1e45347ca72ac3258a32e48eb3a566293e
Hanspeter <hansdietiker2012@gmail.com>
2023-10-10 19:23:53 -0300

    Projeto RocketSeat

---
+++ todolist/src/test/java/com/hanspeter/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanspeter.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
